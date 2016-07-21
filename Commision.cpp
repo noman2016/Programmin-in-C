bf7c3339c1624013c8c86262c210fcfa3a27bd16
Hafez Abdullah Al Noman <hafezalnoman@gmail.com>
2016-07-22 01:26:02 +0600

    Add files via upload

---
+++ Commision.cpp
@@ -0,0 +1,21 @@
+// H A A NOMAN
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    float commision,price,net_price,percentage_commision;
+
+    printf("Please Enter Your Commision = ");
+    scanf("%f",&commision);
+    percentage_commision=100-commision;
+    /*printf("\nYour percentage commision is = %f",percentage_commision);*/
+    printf("\n\nPlease Enter Your Price = ");
+    scanf("%f",&price);
+    net_price=percentage_commision*price/100;
+    printf("\n\nYour Net Price is = %.3f",net_price);
+    printf("\n\n");
+
+getch(); /* Created by Hafez Abdullah Al Noman*/
+}
+
