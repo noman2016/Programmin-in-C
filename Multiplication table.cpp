75d5a99718f7c01c53749834217eca3b4ad074ea
Hafez Abdullah Al Noman <hafezalnoman@gmail.com>
2016-08-09 20:41:27 +0600

    Add files via upload

---
+++ Multiplication table.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int num,mul;
+    printf("Enter your Multiplication table number = ", num);
+    scanf("%d",&num);
+    printf("\n");
+    mul=1* num;
+    printf(" %d * 1 = %d\n",num,mul);
+    mul=2*num;
+    printf(" %d * 2 = %d\n",num,mul);
+    mul=3*num;
+    printf(" %d * 3 = %d\n",num,mul);
+    mul=4* num;
+    printf(" %d * 4 = %d\n",num,mul);
+    mul=5*num;
+    printf(" %d * 5 = %d\n",num,mul);
+    mul=6*num;
+    printf(" %d * 6 = %d\n",num,mul);
+    mul=7* num;
+    printf(" %d * 7 = %d\n",num,mul);
+    mul=8*num;
+    printf(" %d * 8 = %d\n",num,mul);
+    mul=9*num;
+    printf(" %d * 9 = %d\n",num,mul);
+    mul=10*num;
+    printf(" %d * 10 = %d\n\n",num,mul);
+    return 0;
+}
