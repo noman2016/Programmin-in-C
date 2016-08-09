bbc93fb5670b175217f2cb14b94439c34cfdc74a
Hafez Abdullah Al Noman <hafezalnoman@gmail.com>
2016-08-09 19:27:02 +0600

    Add files via upload

---
+++ Fahrenheit to Celsius & Celsius to Fahrenheit.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+    float Fahrenheit, Celsius;
+    printf("Fahrenheit to Celsius\n");
+    printf("\nValue in Fahrenheit : ");
+    scanf("%f",&Fahrenheit);
+    Celsius=5.0/9.0*(Fahrenheit-32);
+    printf("\nValue in Celsius : %f\n",Celsius);
+    printf("\n\n**************************\n\n");
+    printf("Celsius to Fahrenheit\n");
+    printf("\nValue in Celsius : ");
+    scanf("%f",&Celsius);
+    Fahrenheit=32+(Celsius*9/5);
+    printf("\nValue in Fahrenheit : %f\n",Fahrenheit);
+}
