ed3d219f447d84d228271399eddb05bb87668359
Hafez Abdullah Al Noman <hafezalnoman@gmail.com>
2016-08-09 19:41:08 +0600

    Add files via upload

---
+++ odd and even.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    printf("Enter The Number : ", x);
+    scanf("%d",&x);
+
+    if(x %2==0)
+
+        printf("\n The %d number is even \n", x);
+
+    else
+
+        printf("\n The %d number is odd \n\n",x);
+
+}
