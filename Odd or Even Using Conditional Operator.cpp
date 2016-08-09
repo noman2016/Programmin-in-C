7ee88063d3cad7e51c468d17cbfa5fbb3bb25f41
Hafez Abdullah Al Noman <hafezalnoman@gmail.com>
2016-08-09 19:45:12 +0600

    Add files via upload

---
+++ Odd or Even Using Conditional Operator.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+     {   int x;
+                printf("Enter your number: ",x);
+              scanf("%d",&x);
+              printf("Result : ");
+           (x%2==0)?printf("even."):printf("odd.");
+
+
+           getch();
+
+     }
