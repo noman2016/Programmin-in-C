05f7c678635b196cead37b7c8bf224eed65b71c7
Hafez Abdullah Al Noman <hafezalnoman@gmail.com>
2016-09-24 15:54:11 +0600

    Add files via upload

---
+++ Multiplication_Table(loop).cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+//while loop
+{
+        int n,i=1;
+        printf("Enter Your Multiplication Table Number : ");
+        scanf("%d",&n);
+        while (i<=10)
+        {
+                printf("\n%d * %d = %d\n",n,i,n*i);
+                i++;
+        }
+        return 0;
+}
+
+//for loop
+
+/*{
+        int n,i;
+        printf("Enter Your Multiplication Table Number : ");
+        scanf("%d",&n);
+        for (i=1;i<=20;i++)
+        {
+                printf("\n%d * %d = %d\n",n,i,n*i);
+        }
+        return 0;
+}*/
