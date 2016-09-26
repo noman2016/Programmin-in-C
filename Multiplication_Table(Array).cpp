5f5b87c1744c27c42d8ba91bd4759e501df7a14a
Hafez Abdullah Al Noman <hafezalnoman@gmail.com>
2016-09-26 19:51:47 +0600

    Add files via upload

---
+++ Multiplication_Table(Array).cpp
@@ -0,0 +1,33 @@
+//multiplication Table For array
+#include<stdio.h>
+#define row 10
+#define col 10
+main()
+{
+    int r,c;
+    int mul[row][col];
+    printf("\t~~~~~~~~~~~~~~~~~~~~\n");
+    printf("\tMULTIPLICATION TABLE\n");
+    printf("\t~~~~~~~~~~~~~~~~~~~~\n\n");
+    int i,j;
+       printf("No: ");
+    for(j=1;j<=col;j++)
+    {
+        printf("%4d",j);
+    }
+
+    printf("\n---------------------------------------------\n");
+    for(i=0;i<row;i++)
+    {
+        r=i+1;
+        printf("%2d |",r);
+        for(j=1;j<=col;j++)
+        {
+            c=j;
+            mul[i][j]=r*c;
+            printf("%4d",mul[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
